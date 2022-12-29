5a00732feb95a90d954712c07d455b6c01d56e95
MayorlenO <mayorlen.ortega@gmail.com>
2022-12-29 20:05:05 -0300

    Styles

--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
   styleUrls: ['./poke-detail.component.scss']
 })
-export class PokeDetailComponent {
+export class PokeDetailComponent implements OnInit {
+
+  pokemon:any = '';
+  pokemonType = [];
+  pokemonImg = '';
+
+  constructor(private PokemonService: PokemonService, private activateRouter:ActivatedRoute ){
+    this.activateRouter.params.subscribe(
+      params=>{
+        this.getPokemon(params['id'])
+      }
+    );
+  }
+  ngOnInit(): void {
+    
+  }
+
+  getPokemon(id:any){
+    this.PokemonService.getPokemons(id).subscribe(
+      res=>{
+       this.pokemon = res;
+       this.pokemonImg = this.pokemon.sprites.front_default;
+       this.pokemonType = res.types[0].type.name;
+      },
+      err=>{
+        console.log(err);
+      }
+    )
+  }
 
 }
