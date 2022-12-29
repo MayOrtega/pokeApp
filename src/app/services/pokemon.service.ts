184f1c1216a67f069f1b693e0bd9b3878575c8cb
MayorlenO <mayorlen.ortega@gmail.com>
2022-12-29 11:34:33 -0300

    service

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+getPokemons(){
+  return this.http.get<any>(`${this.baseUrl}/pokemon`)
+}
+
 }
